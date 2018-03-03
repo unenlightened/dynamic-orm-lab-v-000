42a4e2f3f95b35e56ae32603c0c3cb00d7543bdf
Chely Ho <adept_inept@yahoo.com>
2018-03-03 14:54:05 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,15 @@ DB[:conn].execute("DROP TABLE IF EXISTS students")
 
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS students (
-  id INTEGER PRIMARY KEY, 
-  name TEXT, 
+  id INTEGER PRIMARY KEY,
+  name TEXT,
   grade INTEGER
   )
 SQL
 
 DB[:conn].execute(sql)
 DB[:conn].results_as_hash = true
+
+def reload!
+  load "environment.rb"
+end
