e9a4cf4783ac3e89decf93d397062552cdc73ae2
Chely Ho <adept_inept@yahoo.com>
2018-03-03 15:24:24 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -40,6 +40,8 @@ class InteractiveRecord
    self.class.column_names.each do |col_name|
      values << "'#{send(col_name)}'" unless send(col_name).nil?
    end
+
+   values.join(', ')
  end
 
  def save
