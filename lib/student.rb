5d49f438b3927559fab3c135751da200e9c8b304
Chely Ho <adept_inept@yahoo.com>
2018-03-03 15:37:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ require 'interactive_record.rb'
 class Student < InteractiveRecord
 
   def attr_accessor
-    self.class.column_names.each {|col_name| attr_accessor col_name.to_sym}
+    self.column_names.each {|col_name| attr_accessor col_name.to_sym}
   end
 
 end
