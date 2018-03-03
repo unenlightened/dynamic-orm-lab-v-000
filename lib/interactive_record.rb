2880d5207cb27c000e1c960a08f4c8d46e7aca0e
Chely Ho <adept_inept@yahoo.com>
2018-03-03 15:42:09 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -42,8 +42,8 @@ class InteractiveRecord
 
  def save
    sql <<-SQL
-     INSERT INTO #{table_name_for_insert} #{col_names_for_insert}
-     VALUES #{values_for_insert}
+     INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+     VALUES (#{values_for_insert})
    SQL
 
    DB[:conn].execute(sql)
